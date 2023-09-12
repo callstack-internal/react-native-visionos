87cd35763d7df02ef58b2a225b6631f782814ecc
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2023-09-12 15:27:43 +0200

    feat: add visionos support

--- packages/react-native/Libraries/AppDelegate/RCTAppDelegate.mm
+++ packages/react-native/Libraries/AppDelegate/RCTAppDelegate.mm
@@ -12,6 +12,7 @@
 #import <react/renderer/runtimescheduler/RuntimeScheduler.h>
 #import "RCTAppSetupUtils.h"
 #import "RCTLegacyInteropComponents.h"
+#import <React/RCTUtils.h>
 
 #if RN_DISABLE_OSS_PLUGIN_HEADER
 #import <RCTTurboModulePlugin/RCTTurboModulePlugin.h>
@@ -49,6 +50,23 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   facebook::react::ContextContainer::Shared _contextContainer;
 }
 @end
+
+#endif
+
+#if TARGET_OS_VISION
+@interface GlassViewController : UIViewController
+
+@end
+
+@implementation GlassViewController
+
+- (UIContainerBackgroundStyle)preferredContainerBackgroundStyle {
+    return UIContainerBackgroundStyleGlass;
+}
+
+@end
+#endif
+
 
 static NSDictionary *updateInitialProps(NSDictionary *initialProps, BOOL isFabricEnabled)
 {
@@ -123,7 +141,13 @@ static NSDictionary *updateInitialProps(NSDictionary *initialProps, BOOL isFabri
 
     rootView = [self createRootViewWithBridge:self.bridge moduleName:self.moduleName initProps:initProps];
   }
+
+#if TARGET_OS_VISION
+  self.window = [[UIWindow alloc] initWithFrame:RCTForegroundWindow().bounds];
+#else
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+#endif
+  
   UIViewController *rootViewController = [self createRootViewController];
   [self setRootView:rootView toRootViewController:rootViewController];
   self.window.rootViewController = rootViewController;
@@ -164,7 +188,11 @@ static NSDictionary *updateInitialProps(NSDictionary *initialProps, BOOL isFabri
 
 - (UIViewController *)createRootViewController
 {
+#if TARGET_OS_VISION
+  return [GlassViewController new];
+#else
   return [UIViewController new];
+#endif
 }
 
 - (void)setRootView:(UIView *)rootView toRootViewController:(UIViewController *)rootViewController
