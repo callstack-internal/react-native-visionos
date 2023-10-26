5686c747528b1ccd0521bf958d110203e7645a7b
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2023-10-26 16:14:38 +0200

    chore: remove not necessary ifs TARGET_OS_VISION, minimise code diff (#22)

--- packages/rn-tester/RNTester/AppDelegate.mm
+++ packages/rn-tester/RNTester/AppDelegate.mm
@@ -12,7 +12,7 @@
 #import <ReactCommon/RCTSampleTurboModule.h>
 #import <ReactCommon/SampleTurboCxxModule.h>
 
-#if !TARGET_OS_TV && !TARGET_OS_UIKITFORMAC && !TARGET_OS_VISION
+#if !TARGET_OS_TV && !TARGET_OS_UIKITFORMAC
 #import <React/RCTPushNotificationManager.h>
 #endif
 
@@ -92,7 +92,7 @@ NSString *kBundlePath = @"js/RNTesterApp.ios";
   return nullptr;
 }
 
-#if !TARGET_OS_TV && !TARGET_OS_UIKITFORMAC && !TARGET_OS_VISION
+#if !TARGET_OS_TV && !TARGET_OS_UIKITFORMAC
 
 // Required for the remoteNotificationsRegistered event.
 - (void)application:(__unused UIApplication *)application
@@ -108,6 +108,7 @@ NSString *kBundlePath = @"js/RNTesterApp.ios";
   [RCTPushNotificationManager didFailToRegisterForRemoteNotificationsWithError:error];
 }
 
+#if !TARGET_OS_VISION
 // Required for the remoteNotificationReceived event.
 - (void)application:(__unused UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)notification
 {
@@ -120,6 +121,7 @@ NSString *kBundlePath = @"js/RNTesterApp.ios";
 {
   [RCTPushNotificationManager didReceiveLocalNotification:notification];
 }
+#endif
 
 #endif
 
