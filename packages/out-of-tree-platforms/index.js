ae757e3a77885b9b94362e28342924190abe8fad
Michał Pierzchała <thymikee@gmail.com>
2023-11-29 14:15:55 +0100

    add comments

--- packages/out-of-tree-platforms/index.js
+++ packages/out-of-tree-platforms/index.js
@@ -14,8 +14,14 @@ type ResolverConfig = {
   platformNameMap: {[platform: string]: string},
 };
 
+/**
+ * Creates a custom Metro resolver that maps platform extensions to package names.
+ * To be used in app's `metro.config.js` as `resolver.resolveRequest`.
+ */
 const getPlatformResolver = (config: ResolverConfig): CustomResolver => {
   return (context, moduleName, platform) => {
+    // `customResolverOptions` is populated through `?resolver.platformExtension` query params
+    // in the jsBundleURLForBundleRoot method of the react-native/React/Base/RCTBundleURLProvider.mm
     const platformExtension = context.customResolverOptions?.platformExtension;
     let modifiedModuleName = moduleName;
 
