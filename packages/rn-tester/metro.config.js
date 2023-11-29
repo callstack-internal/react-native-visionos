bf72cdc0c85a2d5b52d37a767bbae6be98b7f8aa
Michał Pierzchała <thymikee@gmail.com>
2023-11-29 14:16:59 +0100

    fix visionos typo

--- packages/rn-tester/metro.config.js
+++ packages/rn-tester/metro.config.js
@@ -38,7 +38,7 @@ const config = {
       'react-native': path.resolve(__dirname, '../react-native'),
     },
     resolveRequest: getPlatformResolver({
-      platformNameMap: [{visionos: '@callstack/react-native-vision'}],
+      platformNameMap: [{visionos: '@callstack/react-native-visionos'}],
     }),
   },
 };
