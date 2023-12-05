b8793c0b3c9122f5c974580636f7702abe6deeb6
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2023-12-05 20:24:46 +0100

    feat: add visionOS template (#34)
    * Update CLI to v12.1.1
    
    # Conflicts:
    #	packages/community-cli-plugin/package.json
    #	packages/react-native/cli.js
    #	packages/react-native/package.json
    
    * feat: implement visionos react-native template
    
    * feat: modify template look
    
    * feat: add `@callstack/out-of-tree-platforms` to metro config
    
    * fix: metro config
    
    * feat: update supported platforms
    
    ---------
    
    Co-authored-by: szymonrybczak <szymon.rybczak@gmail.com>

--- packages/react-native/cli.js
+++ packages/react-native/cli.js
@@ -10,11 +10,12 @@
 
 'use strict';
 
-const {version: currentVersion} = require('./package.json');
-const cli = require('@react-native-community/cli');
-const chalk = require('chalk');
 const {get} = require('https');
 const {URL} = require('url');
+const chalk = require('chalk');
+const cli = require('@react-native-community/cli');
+
+const {version: currentVersion, name} = require('./package.json');
 
 const isNpxRuntime = process.env.npm_lifecycle_event === 'npx';
 const DEFAULT_REGISTRY_HOST =
@@ -40,7 +41,7 @@ async function getLatestVersion(registryHost = DEFAULT_REGISTRY_HOST) {
 }
 
 /**
- * npx react-native -> @react-native-community/cli
+ * npx react-native -> @react-native-comminity/cli
  *
  * Will perform a version check and warning if you're not running the latest community cli when executed using npx. If
  * you know what you're doing, you can skip this check:
@@ -66,7 +67,7 @@ async function main() {
       // Ignore errors, since it's a nice to have warning
     }
   }
-  return cli.run();
+  return cli.run(name);
 }
 
 if (require.main === module) {
