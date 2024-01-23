9fcec2ef2b9274010c49aa4b6dd578bd0f72a7f8
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-01-23 12:02:17 +0530

    feat: add visionos support

--- packages/react-native/Libraries/PushNotificationIOS/RCTPushNotificationManager.mm
+++ packages/react-native/Libraries/PushNotificationIOS/RCTPushNotificationManager.mm
@@ -94,7 +94,7 @@ RCT_ENUM_CONVERTER(
 
 @end
 #else
-@interface RCTPushNotificationManager () <NativePushNotificationManagerIOSSpec>
+@interface RCTPushNotificationManager ()
 @end
 #endif // TARGET_OS_UIKITFORMAC
 
